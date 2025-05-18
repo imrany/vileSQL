557738a3c8ac05f164fa30d48c4349427d0956d1
imrany <imranmat254@gmail.com>
2025-05-18 18:52:02 +0300

    added a logger, serve static folder and rendering html template, tailwindcss and htmx

--- main.go
+++ main.go
@@ -16,9 +16,9 @@ func main() {
 	r := mux.NewRouter()
 	router.SetupRoutes(r)
 
-	// r.Use(loggingMiddleware)
+	r.Use(loggingMiddleware)
 	// Serve static files for frontend
-	r.PathPrefix("/static").Handler(http.FileServer(http.Dir("./static")))
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
 	// Set up CORS
 	corsHandler := cors.New(cors.Options{
@@ -34,4 +34,12 @@ func main() {
 	}
 	log.Printf("Server running on port %v...", PORT)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf("0.0.0.0:%v", PORT), corsHandler))
+}
+
+// loggingMiddleware logs the incoming HTTP requests
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+		next.ServeHTTP(w, r)
+	})
 }
